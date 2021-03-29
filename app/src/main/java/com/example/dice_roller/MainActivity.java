de9ea52befe012e68609a0cc7746fa39c64c8871
cihaai <78753548+cihaai@users.noreply.github.com>
2021-03-29 11:30:21 -0500

    HW14

--- app/src/main/java/com/example/dice_roller/MainActivity.java
+++ app/src/main/java/com/example/dice_roller/MainActivity.java
@@ -1,28 +1,41 @@
 package com.example.dice_roller;
 
-public class MainActivity : AppCompatActivity
-        {
-            @override
-            private Button B0;
-            private Button B1;
-            private Button B2;
-            private Button B3;
-            private Button B4;
-            private Button B5;
-            private Button B6;
-            private Button B7;
-            private Button B8;
-            private Button B9;
-            private TextView qtyTV;
-        }
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-    protected void onCreate(savedInstanceState: Bundle?)
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private Button B0;
+    private Button B1;
+    private Button B2;
+    private Button B3;
+    private Button B4;
+    private Button B5;
+    private Button B6;
+    private Button B7;
+    private Button B8;
+    private Button B9;
+    private TextView qtyTV;
+
+
+    public void onCreate()
     {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main);
 
-        this.B0 = this.findViewById(R.id.B0)
-        this.qtyTV = this.findViewById(R.id.qtyTV)
+        this.B0 = this.findViewById(R.id.B0);
+        this.B1 = this.findViewById(R.id.B1);
+        this.B2 = this.findViewById(R.id.B2);
+        this.B3 = this.findViewById(R.id.B3);
+        this.B4 = this.findViewById(R.id.B4);
+        this.B5 = this.findViewById(R.id.B5);
+        this.B6 = this.findViewById(R.id.B6);
+        this.B7 = this.findViewById(R.id.B7);
+        this.B8 = this.findViewById(R.id.B8);
+        this.B9 = this.findViewById(R.id.B9)     ;
+        this.qtyTV = this.findViewById(R.id.qtyTV);
 
     }
 
